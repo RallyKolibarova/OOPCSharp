bf7e7b5d136bb16fa70488fb1f139cd18f402f41
RallyKolibarova <sysprogram@abv.bg>
2018-03-13 15:58:13 +0200

    Add files via upload

---
+++ DefineClassPerson.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefineClassPerson
+{
+	class DefineClassPerson
+	{
+		public static void Main (string[] args)
+		{
+
+			var pesho = new Person ("Pesho", 20);
+			var gosho = new Person ("Gosho", 18);
+			var stamat = new Person ("Stamat", 43);
+
+
+			Console.WriteLine (pesho);
+			Console.WriteLine (gosho);
+			Console.WriteLine (stamat);
+
+	}
+}
+}
---
+++ Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefineClassPerson
+{
+	public class Person
+	{
+
+		private string name;
+		private int age;
+
+		public string Name 
+		{
+			get{ return this.name; }
+
+			set{ this.name = value; }
+		}
+
+		public int Age
+		{
+			get{ return this.age; }
+
+			set{ this.age = value; }
+		}
+
+
+		public Person()
+		{
+			this.Age = 1;
+		}
+
+		public Person(int age):this()
+		{
+			this.Name = "No name";
+		}
+
+		public Person(string name, int age)
+		{
+			
+			this.Name = name;
+			this.Age = age;
+
+
+		}
+
+
+		public override string ToString ()
+		{
+			return $"Name \n {Name} \n Age \n {Age}";
+		}
+
+	}
+}
